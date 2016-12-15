7c4455f61779526f0f5921c97bc1247c4e8e0600
Jibin Mathews <jibinmathews7@gmail.com>
2016-12-15 22:13:34 +0530

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "google",
+    "installedESLint": true,
+    "env":{
+      "es6": true,
+      "node": true
+    },
+    "rules":{
+      "semi": "off",
+      "quotes": "off",
+      "comma-dangle": 0,
+      "require-jsdoc": 0,
+      "max-len": 0,
+      "new-cap": 0,
+      "guard-for-in": 0
+    }
+};
